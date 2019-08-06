3e9dc5bdbaae489ae44075865721ba4215089f66
gaopeng <gp626676634@sina.com>
2019-08-06 09:26:50 +0800

    [protoBuf demo]

---
+++ src/main/java/com/gaop/netty/protocolBuf/ProtobufTest.java
@@ -0,0 +1,24 @@
+package com.gaop.netty.protocolBuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+/**
+ * @author gaopeng@doctorwork.com
+ * @description
+ * @date 2019-08-06 8:02
+ **/
+public class ProtobufTest {
+
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        AddressBookProtos.Person person = AddressBookProtos.Person.newBuilder().
+                setName("高鹏").
+                setId(1).
+                setEmail("gp626676634@sina.com").
+                build();
+
+        byte[] person2byteArgs = person.toByteArray();
+        AddressBookProtos.Person person2 = AddressBookProtos.Person.parseFrom(person2byteArgs);
+
+        System.out.println(person2);
+    }
+}
